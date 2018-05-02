92918b4d9b6c3ff04b80488572bbe46e2cf7a039
ogvenkatesh <30319778+omkar-locus@users.noreply.github.com>
2018-05-02 17:04:14 -0500

    fixed loop

--- 313ProjPhase1.py
+++ 313ProjPhase1.py
@@ -53,31 +53,48 @@ pl_vals.insert(len(pl_vals.columns), 'vals', 'medium')
 # mun_counts contains the number of medium bike stations in each mucipality
 mun_counts=pl_vals.groupby('MUN')['vals'].apply(lambda x: (x=='medium').sum()).reset_index(name='num_pls')
 mun_reqs = mun_reqs.merge(mun_counts, left_on='MUN', right_on='MUN', how='inner')
-current_pl = 'p1'
-pl_vals
+
+pl_vals.loc['']
+
 
 for index, row in pl_vals.iterrows():
-    current_mun = pl_vals.loc[pl_vals['pls']==current_pl]['MUN']
-    if mun_reqs.loc[mun_reqs['MUN']==current_mun[1]]['MIN_BIKES']-(mun_reqs.loc[mun_reqs['MUN']==current_mun[1]]['num_pls']-1)*large_size <=0:
+    current_pl = 'p'+str(index)
+    current_mun = pl_vals.loc[pl_vals['pls']==current_pl]['MUN'].iloc[0]
+    condition = mun_reqs.loc[mun_reqs['MUN']==current_mun]['MIN_BIKES']-(mun_reqs.loc[mun_reqs['MUN']==current_mun]['num_pls']-1)*large_size
+    if condition.iloc[0] <=0:
         if pl_vals.loc[pl_vals['vals'] != 'none'].filter(regex="d.*").min(axis=0).max()<=1:
             pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'none'
             dp_paths = greedy_path(dp_list, pl_vals)
+            # remember to update mun_reqs
+            mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']=mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']-1
             if dp_paths['path_time'].max() >.75:
+                #reverting status back to medium
                 pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'medium'
-
+                #reverting mun reqs
+                mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']=mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']+1
 
 pl_vals
 
+# change mediums to large and small as necessary for optimal numbers
+for index, row in mun_reqs.iterrows():
+    cur_min = mun_reqs.ix[index,'MIN_BIKES']
+    cur_max = mun_reqs.ix[index,'MAX_BIKES']
+    num_bikes = pl_vals['vals'].value_counts()['medium']*20
+    if num_bikes < cur_min:
+        # calculate how many to change to large
 
-# remember to update mun_reqs
+    if num_bikes > min:
+        # calculate how many to change to small_cost
 
+mun_reqs
 greedy_path(dp_list,pl_vals)
 
 
 # function takes in DP pair list reduced, pl list, outputs new data frame w/ dp pairs and 2 pls for each forming greedy pathself.
 # loops through each dp pairs
+
 def greedy_path(dp_pairs_list, pl_full_matrix):
-    pl_full_matrix = pl_full_matrix.loc[pl_full_matrix['vals'] != 'none']
+    pl_full_matrix = pl_full_matrix.loc[pl_full_matrix['vals'] != 'none'].reset_index()
     dp_paths = dp_pairs_list[['dp_first','dp_second']]
     dp_paths=dp_paths.assign(pl_first = np.nan)
     dp_paths=dp_paths.assign(pl_second = np.nan)
