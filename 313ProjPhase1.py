d5247bfa7c1b5e4f1dc07e0c1efb57912c8d04bc
ogvenkatesh <30319778+omkar-locus@users.noreply.github.com>
2018-05-02 21:23:35 -0500

    changes

--- 313ProjPhase1.py
+++ 313ProjPhase1.py
@@ -55,53 +55,41 @@ pl_vals.insert(len(pl_vals.columns), 'vals', 'medium')
 mun_counts=pl_vals.groupby('MUN')['vals'].apply(lambda x: (x=='medium').sum()).reset_index(name='num_pls')
 mun_reqs = mun_reqs.merge(mun_counts, left_on='MUN', right_on='MUN', how='inner')
 
-pl_vals.loc['']
 
-
-for index, row in pl_vals.iterrows():
-    current_pl = 'p'+str(index)
+reduced_pls=pl_vals.loc[(pl_vals['special']==0) & (pl_vals['vals'] != 'none')].reset_index()
+violations = (reduced_pls.filter(regex="p\d")<.25).sum(axis=1)
+reduced_pls=reduced_pls.assign(violations = violations)
+current_pl = reduced_pls.loc[reduced_pls.index[reduced_pls['violations'].idxmax()], 'pls']
+#count = sum(violations)
+count=19
+while count > 0:
     # Check that we will not violate municipality minimum
     current_mun = pl_vals.loc[pl_vals['pls']==current_pl]['MUN'].iloc[0]
     condition = mun_reqs.loc[mun_reqs['MUN']==current_mun]['MIN_BIKES']-(mun_reqs.loc[mun_reqs['MUN']==current_mun]['num_pls']-1)*large_size
     if condition.iloc[0] <=0:
+        pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'none'
         # Check that we will not violate 1 mile rule
-        if pl_vals.loc[pl_vals['vals'] != 'none'].filter(regex="d.*").min(axis=0).max()<=1:
-            pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'none'
+        if pl_vals.loc[pl_vals['vals'] != 'none'].filter(regex="d\d").min(axis=0).max()<=dp_proximity:
             dp_paths = greedy_path(dp_list, pl_vals)
             # remember to update mun_reqs
             mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']=mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']-1
             # Check that we will not violate 45 minute time limit
-            if dp_paths['path_time'].max() >.75:
+            if dp_paths['path_time'].max() > .75:
                 #reverting status back to medium
                 pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'medium'
                 #reverting mun reqs
                 mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']=mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']+1
+        else:
+            pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'medium'
+    reduced_pls=pl_vals.loc[(pl_vals['special']==0) & (pl_vals['vals'] != 'none')].reset_index()
+    violations = (reduced_pls.filter(regex="p\d")<.25).sum(axis=1)
+    reduced_pls=reduced_pls.assign(violations = violations)
+    current_pl = reduced_pls.loc[reduced_pls.index[reduced_pls['violations'].idxmax()], 'pls']
+    #count = sum(violations)
+    count = count-1
 
+reduced_pls
 pl_vals
-
-# change mediums to large and small as necessary for optimal numbers
-for index, row in mun_reqs.iterrows():
-    cur_min = mun_reqs.ix[index,'MIN_BIKES']
-    cur_max = mun_reqs.ix[index,'MAX_BIKES']
-    num_bikes = pl_vals['vals'].value_counts()['medium']*20
-    if num_bikes < cur_min:
-        # calculate how many to change to large
-        deficit = cur_min - num_bikes
-        num_to_change = round(0.5 + deficit/(large_size - medium_size))
-        print(num_to_change)
-        # change this many from medium to large_cost
-        while num_to_change < 0:
-             for index, row in pl_vals.loc[pl_vals['vals'] == 'medium'].iterrows():
-                 current_pl = 'p'+str(index)
-                 pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'large'
-
-df.loc[df['column_name'] == some_value]
-    if num_bikes > min:
-        # calculate how many to change to small_cost
-pl_vals.loc[pl_vals['vals'] == 'medium']
-mun_reqs
-greedy_path(dp_list,pl_vals)
-num_bikes
 
 # function takes in DP pair list reduced, pl list, outputs new data frame w/ dp pairs and 2 pls for each forming greedy pathself.
 # loops through each dp pairs
@@ -122,6 +110,73 @@ def greedy_path(dp_pairs_list, pl_full_matrix):
         dp_paths.loc[index, 'pl_second'] = pl_s
         # Calculate time for each path
         dp_paths.loc[index, 'path_time'] = pl_full_matrix.loc[(pl_full_matrix['pls'] == pl_f), dp_f].values/walk_speed + pl_full_matrix.loc[(pl_full_matrix['pls'] == pl_f), pl_s].values/bike_speed + pl_full_matrix.loc[(pl_full_matrix['pls'] == pl_s), dp_s].values/walk_speed
-
     return(dp_paths)
     #
+
+
+
+for index, row in pl_vals.iterrows():
+    current_pl = 'p'+str(index)
+    # Check that we will not violate municipality minimum
+    current_mun = pl_vals.loc[pl_vals['pls']==current_pl]['MUN'].iloc[0]
+    condition = mun_reqs.loc[mun_reqs['MUN']==current_mun]['MIN_BIKES']-(mun_reqs.loc[mun_reqs['MUN']==current_mun]['num_pls']-1)*large_size
+    if condition.iloc[0] <=0:
+        pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'none'
+        # Check that we will not violate 1 mile rule
+        if pl_vals.loc[pl_vals['vals'] != 'none'].filter(regex="d\d").min(axis=0).max()<=dp_proximity:
+            dp_paths = greedy_path(dp_list, pl_vals)
+            # remember to update mun_reqs
+            mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']=mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']-1
+            # Check that we will not violate 45 minute time limit
+            if dp_paths['path_time'].max() > .75:
+                #reverting status back to medium
+                pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'medium'
+                #reverting mun reqs
+                mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']=mun_reqs.loc[mun_reqs['MUN']==current_mun,'num_pls']+1
+        else:
+            pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'medium'
+
+
+# change mediums to large and small as necessary for optimal numbers
+# This loop changes mediums to larges until minimum is reached or exceeded
+for index, row in mun_reqs.iterrows():
+    cur_min = mun_reqs.iloc[index,mun_reqs.columns.get_loc('MIN_BIKES')]
+    num_bikes = pl_vals.loc[pl_vals['MUN'] == index]['vals'].value_counts()['medium']*medium_size
+    if num_bikes < cur_min:
+        # calculate how many to change to large
+        deficit = cur_min - num_bikes
+        num_to_change = round(0.5 + deficit/(large_size - medium_size))
+        print(num_to_change)
+        # change this many from medium to large_cost
+        for index, row in pl_vals.loc[pl_vals['vals'] == 'medium'].iterrows():
+            current_pl = 'p'+str(index)
+            pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'large'
+            num_to_change -= 1
+            if num_to_change <= 0:
+                break
+# Next this loop changes mediums into smalls to be as close to minimum as possible or until all have been changed
+for index, row in mun_reqs.iterrows():
+    cur_min = mun_reqs.iloc[index, mun_reqs.columns.get_loc('MIN_BIKES')]
+    # THIS NEXT LINE DOESNT LIKE WHEN I CHANGE 'medium' TO 'large'
+    num_bikes = pl_vals.loc[pl_vals['MUN'] == index]['vals'].value_counts()['medium']*medium_size + pl_vals.loc[pl_vals['MUN'] == index]['vals'].value_counts()['large']*large_size
+    if num_bikes > cur_min:
+        # calculate how many to change to small_cost
+        excess = num_bikes - cur_min
+        num_to_change = int(excess/(medium_size - small_size))
+        print(num_to_change)
+        # Change this many from medium to small
+        for index, row in pl_vals.loc[pl_vals['vals'] == 'medium'].iterrows():
+            current_pl = 'p'+str(index)
+            pl_vals.loc[pl_vals['pls']==current_pl, 'vals'] = 'small'
+            num_to_change -= 1
+            if num_to_change <= 0:
+                break
+
+index
+pl_vals.loc[pl_vals['MUN'] == index]
+
+pl_vals.loc[pl_vals['vals'] == 'medium']
+mun_reqs
+greedy_path(dp_list,pl_vals)
+num_bikes
+pl_vals
